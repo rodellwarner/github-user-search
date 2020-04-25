87039ec167b81aedbe2a3528f1a51b0467b2feae
Rodell Warner <rodell.warner@gmail.com>
2020-04-24 21:19:17 -0500

    it works

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 "use strict"
 
-
-
-
-
 function handleSubmit() {
   $('#form').submit(function() {
     event.preventDefault();
+    $('.results').empty();
     getUserRepos();
-    console.log('handleSubmit ran');
   })
 }
 
 function getUserRepos() {
   let userName = $('#textInput').val();
-  console.log(userName);
-  console.log('getUserRepos ran');
   fetch('https://api.github.com/users/' + userName + '/repos')
   .then(function(response) {
     return response.json();
   })
   .then(function (repos) {
     const repoInfo = repos.map(function(repo) {
-      
+      $('.results').append(`<li><a href="${repo.html_url}">${repo.name}</a></li>`);
     })
-  });
+  })
 }
 
-
-
 function handleGithubUserSearch() {
-  console.log('handleGithubUserSearch ran');
   handleSubmit();
 }
 
